96e45c6d96e86af02bc219d9786a411e77c43154
SW Test Academy <swtestacademy@gmail.com>
2017-02-07 00:33:55 +0300

    Code structure changed. BaseTest Class and JSWaiter Class added.

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,297 @@
+import com.google.common.io.Files;
+import org.apache.commons.io.FileUtils;
+import org.im4java.core.CompareCmd;
+import org.im4java.core.IMOperation;
+import org.im4java.process.ProcessStarter;
+import org.im4java.process.StandardStream;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import ru.yandex.qatools.ashot.AShot;
+import ru.yandex.qatools.ashot.Screenshot;
+
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by onurb on 06-Feb-17.
+ */
+public class BaseTest {
+
+    public WebDriver driver;
+    public WebDriverWait wait;
+    public JavascriptExecutor js;
+
+    //JSWaiter object
+    JSWaiter jsWaiter;
+
+    //Test name
+    public String testName;
+
+    //Test Screenshot directory
+    public String testScreenShotDirectory;
+
+    //URL of the test website
+    public String url = "http://www.kariyer.net";
+
+    //Main Directory of the test code
+    public String currentDir = System.getProperty("user.dir");
+
+    //Main screenshot directory
+    public String parentScreenShotsLocation = currentDir + "\\ScreenShots\\";
+
+    //Main differences directory
+    public String parentDifferencesLocation = currentDir + "\\Differences\\";
+
+    //Element screenshot paths
+    public String baselineScreenShotPath;
+    public String actualScreenShotPath;
+    public String differenceScreenShotPath;
+
+    //Image files
+    public File baselineImageFile;
+    public File actualImageFile;
+    public File differenceImageFile;
+    public File differenceFileForParent;
+
+    //Setup Driver
+    @BeforeClass
+    public void setupTestClass() throws IOException {
+        //Declare Firefox driver
+        driver = new ChromeDriver();
+
+        //Maximize the browser
+        driver.manage().window().maximize();
+
+        //Declare a 10 seconds wait time
+        wait = new WebDriverWait(driver,10);
+
+        //JS Executor
+        js = (JavascriptExecutor) driver;
+
+        //JSWaiter
+        jsWaiter = new JSWaiter(wait);
+
+        //Create screenshot and differences folders if they are not exist
+        createFolder(parentScreenShotsLocation);
+        createFolder(parentDifferencesLocation);
+
+        //Clean Differences Root Folder
+        File differencesFolder = new File(parentDifferencesLocation);
+        FileUtils.cleanDirectory(differencesFolder);
+
+        //Go to URL
+        driver.navigate().to(url);
+
+        //Add Cookie for top banner
+        addCookieforTopBanner();
+    }
+
+    @BeforeMethod
+    public void setupTestMethod (Method method) {
+        //Get the test name to create a specific screenshot folder for each test.
+        testName = method.getName();
+        System.out.println("Test Name: " + testName + "\n");
+
+        //Create a specific directory for a test
+        testScreenShotDirectory = parentScreenShotsLocation + testName + "\\";
+        createFolder(testScreenShotDirectory);
+
+        //Declare element screenshot paths
+        //Concatenate with the test name.
+        declareScreenShotPaths(testName+"_Baseline.png", testName+"_Actual.png", testName + "_Diff.png");
+    }
+
+    //Add Cookie not to see top banner animation
+    public void addCookieforTopBanner () {
+        //Get Next Month Last Date for cookie expiration
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.MONTH, 1);
+        calendar.set(Calendar.DATE, calendar.getActualMaximum(Calendar.DAY_OF_MONTH));
+        Date nextMonthLastDay = calendar.getTime();
+
+        //Create/Build a cookie
+        Cookie topBannerCloseCookie = new Cookie.Builder("AA-kobiBannerClosed","4") //Name & value pair of the cookie
+                .domain("www.kariyer.net") //Domain of the cookie
+                .path("/") //Path of the cookie
+                .expiresOn(nextMonthLastDay) //Expiration date
+                .build(); //Finally build it with .build() call
+
+        //Add a cookie
+        driver.manage().addCookie(topBannerCloseCookie);
+    }
+
+    //Create Folder Method
+    public void createFolder (String path) {
+        File testDirectory = new File(path);
+        if (!testDirectory.exists()) {
+            if (testDirectory.mkdir()) {
+                System.out.println("Directory is created!");
+            } else {
+                System.out.println("Failed to create directory!");
+            }
+        } else {
+            System.out.println("Directory already exists: " + path);
+        }
+    }
+
+    //Close popup if exists
+    public void handlePopup(String selector) throws InterruptedException {
+        jsWaiter.waitJS();
+        List<WebElement> popup = driver.findElements(By.cssSelector(selector));
+        if(!popup.isEmpty()){
+            popup.get(0).click();
+            sleep(200);
+        }
+    }
+
+    //Close Banner
+    public void closeBanner () {
+        jsWaiter.waitJS();
+        List<WebElement> banner = driver.findElements(By.cssSelector("body > div.kobi-head-banner > div > a"));
+        if (!banner.isEmpty()) {
+            banner.get(0).click();
+            //Wait for 2 second for closing banner
+            sleep(2000);
+        }
+    }
+
+    //Unhide an Element with JSExecutor
+    public void unhideElement (String unhideJS) {
+        js.executeScript(unhideJS);
+        jsWaiter.waitJS();
+        sleep(200);
+    }
+
+    //Move to Operation
+    public void moveToElement (WebElement element){
+        jsWaiter.waitJS();
+        Actions actions = new Actions(driver);
+        jsWaiter.waitJS();
+        sleep(200);
+        actions.moveToElement(element).build().perform();
+    }
+
+    //Take Screenshot with AShot
+    public Screenshot takeScreenshot (WebElement element) {
+        //Take screenshot with Ashot
+        Screenshot elementScreenShot = new AShot().takeScreenshot(driver, element);
+        /*Screenshot elementScreenShot = new AShot()
+                .coordsProvider(new WebDriverCoordsProvider())
+                .takeScreenshot(driver,element);*/
+
+        //Print element size
+        String size = "Height: " + elementScreenShot.getImage().getHeight() + "\n" +
+                "Width: " + elementScreenShot.getImage().getWidth() + "\n";
+        System.out.print("Size: " + size);
+
+        return elementScreenShot;
+    }
+
+    //Write
+    public void writeScreenshotToFolder (Screenshot screenshot, File imageFile) throws IOException {
+        ImageIO.write(screenshot.getImage(), "PNG", imageFile);
+    }
+
+    //Screenshot paths
+    public void declareScreenShotPaths (String baseline, String actual, String diff) {
+        //BaseLine, Actual, Difference Photo Paths
+        baselineScreenShotPath = testScreenShotDirectory + baseline;
+        actualScreenShotPath = testScreenShotDirectory + actual;
+        differenceScreenShotPath = testScreenShotDirectory + diff;
+
+        //BaseLine, Actual Photo Files
+        baselineImageFile = new File(baselineScreenShotPath);
+        actualImageFile = new File(actualScreenShotPath);
+        differenceImageFile = new File (differenceScreenShotPath);
+
+        //For copying difference to the parent Difference Folder
+        differenceFileForParent = new File (parentDifferencesLocation + diff);
+    }
+
+    //ImageMagick Compare Method
+    public void compareImagesWithImageMagick (String expected, String actual, String difference) throws Exception {
+        // This class implements the processing of os-commands using a ProcessBuilder.
+        // This is the core class of the im4java-library where all the magic takes place.
+        ProcessStarter.setGlobalSearchPath("C:\\Program Files\\ImageMagick-7.0.4-Q16");
+
+        // This instance wraps the compare command
+        CompareCmd compare = new CompareCmd();
+
+        // Set the ErrorConsumer for the stderr of the ProcessStarter.
+        compare.setErrorConsumer(StandardStream.STDERR);
+
+        // Create ImageMagick Operation Object
+        IMOperation cmpOp = new IMOperation();
+
+        //Add option -fuzz to the ImageMagick commandline
+        //With Fuzz we can ignore small changes
+        cmpOp.fuzz(10.0);
+
+        //The special "-metric" setting of 'AE' (short for "Absolute Error" count), will report (to standard error),
+        //a count of the actual number of pixels that were masked, at the current fuzz factor.
+        cmpOp.metric("AE");
+
+        // Add the expected image
+        cmpOp.addImage(expected);
+
+        // Add the actual image
+        cmpOp.addImage(actual);
+
+        // This stores the difference
+        cmpOp.addImage(difference);
+
+        try {
+            //Do the compare
+            System.out.println ("Comparison Started!");
+            compare.run(cmpOp);
+            System.out.println ("Comparison Finished!");
+        }
+        catch (Exception ex) {
+            System.out.print(ex);
+            System.out.println ("Comparison Failed!");
+            //Put the difference image to the global differences folder
+            Files.copy(differenceImageFile,differenceFileForParent);
+            throw ex;
+        }
+    }
+
+    //Compare Operation
+    public void doComparison (Screenshot elementScreenShot) throws Exception {
+        //Did we capture baseline image before?
+        if (baselineImageFile.exists()){
+            //Compare screenshot with baseline
+            System.out.println("Comparison method will be called!\n");
+
+            System.out.println("Baseline: " + baselineScreenShotPath + "\n" +
+                    "Actual: " + actualScreenShotPath + "\n" +
+                    "Diff: " + differenceScreenShotPath);
+
+            //Try to use IM4Java for comparison
+            compareImagesWithImageMagick(baselineScreenShotPath, actualScreenShotPath, differenceScreenShotPath);
+        } else {
+            System.out.println("BaselineScreenshot is not exist! We put it into test screenshot folder.\n");
+            //Put the screenshot to the specified folder
+            ImageIO.write(elementScreenShot.getImage(), "PNG", baselineImageFile);
+        }
+    }
+
+    //Sleep Function
+    public void sleep (int milis) {
+        Long milliseconds = (long) milis;
+        try {
+            Thread.sleep(milliseconds);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    
+}
--- src/test/java/KariyerVisualTest.java
+++ src/test/java/KariyerVisualTest.java
@@ -1,331 +1,39 @@
-import org.apache.commons.io.FileUtils;
-import org.im4java.core.CompareCmd;
-import org.im4java.core.IMOperation;
-import org.im4java.process.ProcessStarter;
-import org.im4java.process.StandardStream;
-import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import ru.yandex.qatools.ashot.AShot;
 import ru.yandex.qatools.ashot.Screenshot;
-import ru.yandex.qatools.ashot.coordinates.WebDriverCoordsProvider;
-
-import javax.imageio.ImageIO;
-import java.io.File;
-import java.io.IOException;
-import java.lang.reflect.Method;
-import com.google.common.io.Files;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
 
 /**
  * Created by onurb on 28-Aug-16.
  */
-public class KariyerVisualTest {
-
-    private WebDriver driver;
-    private WebDriverWait wait;
-    private JavascriptExecutor js;
-
-
-    //URL of the test website
-    private String url = "http://www.kariyer.net";
-
-    //Main Directory of the test code
-    private String currentDir = System.getProperty("user.dir");
-
-    //Main screenshot directory
-    private String parentScreenShotsLocation = currentDir + "\\ScreenShots\\";
-
-    //Main differences directory
-    private String parentDifferencesLocation = currentDir + "\\Differences\\";
-
-    //Test name
-    private String testName;
-
-    //Test Screenshot directory
-    private String testScreenShotDirectory;
-
-    //Element screenshot paths
-    private String baselineScreenShotPath;
-    private String actualScreenShotPath;
-    private String differenceScreenShotPath;
-
-    //Image files
-    public File baselineImageFile;
-    public File actualImageFile;
-    public File differenceImageFile;
-    public File differenceFileForParent;
-
-    //Setup Driver
-    @BeforeClass
-    public void setupTestClass() {
-        //Declare Firefox driver
-        driver = new ChromeDriver();
-
-        //Go to URL
-        driver.navigate().to(url);
-
-        //Maximize the browser
-        driver.manage().window().maximize();
-
-        //Declare a 10 seconds wait time
-        wait = new WebDriverWait(driver,10);
-
-        //JS Executor
-        js = (JavascriptExecutor) driver;
-
-        //Create screenshot and differences folders
-        createFolder(parentScreenShotsLocation);
-        createFolder(parentDifferencesLocation);
-
-        //Clean Differences Root Folder
-        File differencesFolder = new File(parentDifferencesLocation);
-        try {
-            FileUtils.cleanDirectory(differencesFolder);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        //Add Cookie for top banner
-        addCookieforTopBanner();
-    }
-
-    @BeforeMethod
-    public void setupTestMethod(Method method) {
-        //Get the test name to create a specific screenshot folder for each test.
-        testName = method.getName();
-        System.out.println("Test Name: " + testName + "\n");
-
-        //Create a specific directory for a test
-        testScreenShotDirectory = parentScreenShotsLocation + testName + "\\";
-        createFolder(testScreenShotDirectory);
-
-        wait = new WebDriverWait(driver,10);
-
-    }
-
-    //Add Cookie not to see top banner animation
-    private void addCookieforTopBanner () {
-        //Get Next Month Last Date for cookie expiration
-        Calendar calendar = Calendar.getInstance();
-        calendar.add(Calendar.MONTH, 1);
-        calendar.set(Calendar.DATE, calendar.getActualMaximum(Calendar.DAY_OF_MONTH));
-        Date nextMonthLastDay = calendar.getTime();
-
-        //Create/Build a cookie
-        Cookie topBannerCloseCookie = new Cookie.Builder("AA-kobiBannerClosed","4") //Name & value pair of the cookie
-                .domain("www.kariyer.net") //Domain of the cookie
-                .path("/") //Path of the cookie
-                .expiresOn(nextMonthLastDay) //Expiration date
-                .build(); //Finally build it with .build() call
-
-        //Add a cookie
-        driver.manage().addCookie(topBannerCloseCookie);
-    }
-
-    //Create Folder Method
-    private void createFolder (String path) {
-        File testDirectory = new File(path);
-        if (!testDirectory.exists()) {
-            if (testDirectory.mkdir()) {
-                System.out.println("Directory is created!");
-            } else {
-                System.out.println("Failed to create directory!");
-            }
-        } else {
-            System.out.println("Directory already exists: " + path);
-        }
-    }
-
-    //Close popup if exists
-    private void handlePopup(String selector) {
-        List<WebElement> popup = driver.findElements(By.cssSelector(selector));
-        if(!popup.isEmpty()){
-            popup.get(0).click();
-        }
-    }
-
-    //Take Screenshot with AShot
-    private Screenshot takeScreenshot (WebElement element) {
-        //Take screenshot with Ashot
-        //AShot JQuery screenshot capture is not working. Thus, I used webdriver's CoordsProvider method.
-        Screenshot elementScreenShot = new AShot().takeScreenshot(driver, element);
-/*        Screenshot elementScreenShot = new AShot()
-                .coordsProvider(new WebDriverCoordsProvider())
-                .takeScreenshot(driver,element);*/
-
-        //Print element size
-        String size = "Height: " + elementScreenShot.getImage().getHeight() + "\n" +
-                      "Width: " + elementScreenShot.getImage().getWidth() + "\n";
-        System.out.print("Size: " + size);
-
-        return elementScreenShot;
-    }
-
-    //Screenshot paths
-    private void declareScreenShotPaths (String baseline, String actual, String diff) {
-        //BaseLine, Current, Difference Photo Paths
-        baselineScreenShotPath = testScreenShotDirectory + baseline;
-        actualScreenShotPath = testScreenShotDirectory + actual;
-        differenceScreenShotPath = testScreenShotDirectory + diff;
-
-
-        //BaseLine, Current Photo Files
-        baselineImageFile = new File(baselineScreenShotPath);
-        actualImageFile = new File(actualScreenShotPath);
-        differenceImageFile = new File (differenceScreenShotPath);
-
-        //For copying difference to the parent Difference Folder
-        differenceFileForParent = new File (parentDifferencesLocation + diff);
-    }
-
-    //ImageMagick Compare Method
-    private boolean compareImagesWithImageMagick (String exp, String cur, String diff) throws Exception {
-        // This class implements the processing of os-commands using a ProcessBuilder.
-        // This is the core class of the im4java-library where all the magic takes place.
-        // It does add some overhead compared to a direct call of ProcessBuilder,
-        // but you gain additional features like piping and asynchronous execution.
-        ProcessStarter.setGlobalSearchPath("C:\\Program Files\\ImageMagick-7.0.4-Q16");
-
-        // This instance wraps the compare command
-        CompareCmd compare = new CompareCmd();
-
-        // Set the ErrorConsumer for the stderr of the ProcessStarter.
-        compare.setErrorConsumer(StandardStream.STDERR);
-
-        // Create ImageMagick Operation Object
-        IMOperation cmpOp = new IMOperation();
-
-        //Add option -fuzz to the ImageMagick commandline
-        cmpOp.fuzz(10.0);
-        cmpOp.metric("AE");
-
-        // Add the expected image
-        cmpOp.addImage(exp);
-
-        // Add the current image
-        cmpOp.addImage(cur);
-
-        // This stores the difference
-        cmpOp.addImage(diff);
-
-        try {
-            //Do the compare
-            System.out.println ("Comparison Started!");
-            compare.run(cmpOp);
-            System.out.println ("Comparison Finished!");
-            //DisplayCmd.show(diff);
-            return true;
-        }
-        catch (Exception ex) {
-            System.out.print(ex);
-            System.out.println ("Comparison Failed!");
-            //Put the difference also difference folder
-            Files.copy(differenceImageFile,differenceFileForParent);
-            throw ex;
-        }
-    }
-
-    //Compare Operation
-    private void doComparison (Screenshot elementScreenShot) throws Exception {
-        //Did we capture baseline image before?
-        if (baselineImageFile.exists()){
-            //Compare screenshot with baseline
-            System.out.println("Comparison method will be called!\n");
-
-            System.out.println("Baseline: " + baselineScreenShotPath + "\n" +
-                    "Actual: " + actualScreenShotPath + "\n" +
-                    "Diff: " + differenceScreenShotPath);
-
-            //Try to use IM API for comparison
-            boolean compareResult = compareImagesWithImageMagick(baselineScreenShotPath, actualScreenShotPath, differenceScreenShotPath);
-
-            //If comparison result true print that it passed.
-            if(compareResult==true) {
-                System.out.println ("Comparison Passed!");
-            }
-
-        } else {
-            System.out.println("BaselineScreenshot is not exist! We put it into test screenshot folder.\n");
-            //Put the screenshot to the specified folder
-            ImageIO.write(elementScreenShot.getImage(), "PNG", baselineImageFile);
-        }
-    }
+public class KariyerVisualTest extends BaseTest {
 
     @Test
     public void kariyerUzmanCssTest () throws Exception {
-        //Wait for Javascript to load
-        ExpectedCondition<Boolean> jsLoad = driver -> ((JavascriptExecutor) driver)
-                .executeScript("return document.readyState").toString().equals("complete");
-        wait.until(jsLoad);
-
-        //JQuery Wait
-        ExpectedCondition<Boolean> jQueryLoad = driver -> ((Long) ((JavascriptExecutor) driver)
-                .executeScript("return jQuery.active") == 0);
-
-        wait.until(jsLoad);
-        wait.until(jQueryLoad);
-
         //Handle popup
         handlePopup(".ui-dialog-titlebar-close");
 
-        wait.until(jsLoad);
-        wait.until(jQueryLoad);
-        Thread.sleep(200);
-
-        //Close Banner
-        try {
-            List<WebElement> banner = driver.findElements(By.cssSelector("body > div.kobi-head-banner > div > a"));
-            if(!banner.isEmpty()) {
-                banner.get(0).click();
-                //Wait for 2 second for closing banner
-                wait.until(jsLoad);
-                wait.until(jQueryLoad);
-                Thread.sleep(2000);
-            }
-        } catch (Exception e) {
-            System.out.println("Banner Closed Before!");
-        }
+        //Close banner
+        closeBanner();
 
         //Declare UZMAN photo section
         WebElement uzmanPhotoSection = driver.findElement(By.cssSelector(".item.uzman>a"));
 
         //Unhide Text (It is Changing A lot)
-        js.executeScript("document.getElementsByClassName('count')[0].style.display='none';");
-        wait.until(jsLoad);
-        wait.until(jQueryLoad);
-        Thread.sleep(200);
+        unhideElement("document.getElementsByClassName('count')[0].style.display='none';");
 
         //Move To Operation
-        Actions actions = new Actions(driver);
-        wait.until(jsLoad);
-        wait.until(jQueryLoad);
-        Thread.sleep(200);
-        actions.moveToElement(uzmanPhotoSection).build().perform();
+        moveToElement(uzmanPhotoSection);
 
         //Wait for 2 second for violet color animation
-        wait.until(jsLoad);
-        wait.until(jQueryLoad);
         Thread.sleep(2000);
 
         //Take ScreenShot with AShot
         Screenshot uzmanScreenShot = takeScreenshot(uzmanPhotoSection);
 
-        //Declare element screenshot paths
-        //Concatenate with the test name.
-        declareScreenShotPaths(testName+"_Baseline.png", testName+"_Actual.png", testName + "_Diff.png");
-
-        //Write actual image to the test screenshot path
-        ImageIO.write(uzmanScreenShot.getImage(), "PNG", actualImageFile);
+        //Write actual screenshot to the actual screenshot path
+        writeScreenshotToFolder(uzmanScreenShot, actualImageFile);
 
         //Do image comparison
         doComparison(uzmanScreenShot);
