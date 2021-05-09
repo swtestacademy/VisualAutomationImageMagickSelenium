8ed26f6117f6d5f9f7d0ef4caa14e73f306ec8c6
Onur Baskirt <onur.baskirt@dnata.com>
2021-05-09 07:57:18 +0400

    comparison threshold changed.

--- src/test/java/utils/ImageMagickUtil.java
+++ src/test/java/utils/ImageMagickUtil.java
@@ -25,7 +25,7 @@ public class ImageMagickUtil {
 
         //Add option -fuzz to the ImageMagick commandline
         //With Fuzz we can ignore small changes
-        cmpOp.fuzz(5.0);
+        cmpOp.fuzz(8.0);
 
         //The special "-metric" setting of 'AE' (short for "Absolute Error" count), will report (to standard error),
         //a count of the actual number of pixels that were masked, at the current fuzz factor.
